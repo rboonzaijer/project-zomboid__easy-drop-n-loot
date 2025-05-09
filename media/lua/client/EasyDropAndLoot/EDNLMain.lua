c09c3b00bbdb8bcdefe47817ae6641f24198419b
Roel <10514742+rboonzaijer@users.noreply.github.com>
2025-05-09 14:57:52 +0200

    support rootCategories for mod 'Better Sorting' - https://steamcommunity.com/sharedfiles/filedetails/?id=2313387159

--- media/lua/client/EasyDropAndLoot/EDNLMain.lua
+++ media/lua/client/EasyDropAndLoot/EDNLMain.lua
@@ -3,6 +3,29 @@
 require "TimedActions/ISTimedActionQueue"
 require "TimedActions/ISInventoryTransferAction"
 
+-- Trim whitespaces from the beginning + end of a string
+local function trim(s)
+  return (string.gsub(s, "^%s*(.-)%s*$", "%1"))
+end
+
+-- Split a string by delimiter
+local function split(str, delimiter)
+    local table = {}
+    for k, v in string.gmatch(str, "([^" .. delimiter .. "]+)")
+    do
+        table[#table+1] = k
+    end
+    return table
+end
+
+--- Get Category or Subcategory name for comparison (depending on onlySubCategory)
+local function getCategoryComparisonName(category, onlySubCategory)
+    if onlySubCategory == false then
+        return trim(split(category, "-")[1])
+    end
+    return category
+end
+
 -- Convert array into table
 local function arrayToTable(array)
     if (type(array) == "table") then
@@ -55,7 +78,8 @@ local function EDNL_getItemsCategories(items)
     local list = arrayToTable(items)
     for _, selectedItem in ipairs(list) do
         local item = getSingleItem(selectedItem)
-        local category = getItemCategory(item)
+        -- local category = getItemCategory(item)
+        local category = getItemDisplayCategory(item)
         if (category ~= nil and categories[category] == nil) then
             categories[category] = true
             table.insert(result, category)
@@ -65,12 +89,13 @@ local function EDNL_getItemsCategories(items)
 end
 
 -- Get items of specific category
-local function getItemsByCategory(inventory, category)
+local function getItemsByCategory(inventory, category, onlySubCategory)
     local result = {}
     local items = inventory:getItems()
     for i = 0, items:size() - 1 do
         local item = items:get(i)
-        if (getItemCategory(item) == category) then
+        -- if (getItemCategory(item) == category) then
+        if (getCategoryComparisonName(getItemDisplayCategory(item), onlySubCategory) == getCategoryComparisonName(category, onlySubCategory)) then
             table.insert(result, item)
         end
     end
@@ -78,11 +103,11 @@ local function getItemsByCategory(inventory, category)
 end
 
 -- Drop items of selected categories form selected player inventory
-function EDNL_moveItemsByCategories(items, playerIndex, source, destination)
+function EDNL_moveItemsByCategories(items, playerIndex, source, destination, onlySubCategory)
     local player = getSpecificPlayer(playerIndex)
     local categories = EDNL_getItemsCategories(items) -- get same category items from player's inventory
     for _, category in ipairs(categories) do
-        local targetItems = getItemsByCategory(source, category) -- get same category items from player's inventory
+        local targetItems = getItemsByCategory(source, category, onlySubCategory) -- get same category items from player's inventory
         for _, inventoryItem in pairs(targetItems) do
             local item = getSingleItem(inventoryItem)
             -- ignore favorite and equipped items
@@ -95,19 +120,20 @@ function EDNL_moveItemsByCategories(items, playerIndex, source, destination)
 end
 
 -- Transfer items from source container to destination container
-local function transfer(playerIndex, source, destination)
+local function transfer(playerIndex, source, destination, onlySubCategory)
     local items = destination:getItems() -- items in destination inventory
-    EDNL_moveItemsByCategories(items, playerIndex, source, destination)
+
+    EDNL_moveItemsByCategories(items, playerIndex, source, destination, onlySubCategory)
 end
 
 --- Get categories display names list of selected items
-function EDNL_getItemsDisplayCategories(items)
+function EDNL_getItemsDisplayCategories(items, onlySubCategory)
     local result = {}
     local categories = {}
     local list = arrayToTable(items)
     for _, selectedItem in ipairs(list) do
         local item = getSingleItem(selectedItem)
-        local category = getItemDisplayCategory(item)
+        local category = getCategoryComparisonName(getItemDisplayCategory(item), onlySubCategory)
         if (category ~= nil and categories[category] == nil) then
             categories[category] = true
             table.insert(result, category)
@@ -145,17 +171,17 @@ function EDNL_getSourceAndDestinationInventories(selectedItems, playerIndex)
 end
 
 -- Drop items form selected player inventory
-function EDNL_dropItems(playerIndex)
+function EDNL_dropItems(playerIndex, onlySubCategory)
     local playerData = getPlayerData(playerIndex)
     local playerInventory = playerData.playerInventory.inventory -- currently selected player container (e.g., player's duffel bag, plastic bag, key ring, own inventory etc.)
     local lootInventory = playerData.lootInventory.inventory -- currently selected loot container (e.g., shelf, fridge, crate, floor etc.)
-    transfer(playerIndex, playerInventory, lootInventory)
+    transfer(playerIndex, playerInventory, lootInventory, onlySubCategory)
 end
 
 -- Loot items from selected loot container
-function EDNL_lootItems(playerIndex)
+function EDNL_lootItems(playerIndex, onlySubCategory)
     local playerData = getPlayerData(playerIndex)
     local playerInventory = playerData.playerInventory.inventory -- currently selected player container (e.g., player's duffel bag, plastic bag, key ring, own inventory etc.)
     local lootInventory = playerData.lootInventory.inventory -- currently selected loot container (e.g., shelf, fridge, crate, floor etc.)
-    transfer(playerIndex, lootInventory, playerInventory)
+    transfer(playerIndex, lootInventory, playerInventory, onlySubCategory)
 end
